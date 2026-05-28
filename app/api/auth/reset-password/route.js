0c1237781fc27bf5bb4a11180703812c0bc62660
adityarajnetajigaikwad <adityarajgaikwad999@gmail.com>
2026-05-28 18:34:06 +0530

    fixed mysql database import path

--- app/api/auth/reset-password/route.js
+++ app/api/auth/reset-password/route.js
@@ -1,88 +1,56 @@
 import { NextResponse } from "next/server";
-
 import bcrypt from "bcryptjs";
-
 import User from "@/models/User";
 
-import { connectDB } from "@/lib/db";
+import { connectDB } from "@/lib/mysql";
 
 export async function POST(req) {
-
   try {
-
     await connectDB();
 
-    const {
-      email,
-      otp,
-      newPassword,
-    } = await req.json();
-
-    // find user
-    const user =
-      await User.findOne({ email });
+    const { email, otp, newPassword } = await req.json();
+    const user = await User.findOne({ email });
 
     if (!user) {
-
       return NextResponse.json({
-
         success: false,
         message: "User not found",
-
       });
-
     }
 
     // check otp
     if (user.otp !== otp) {
-
       return NextResponse.json({
-
         success: false,
         message: "Invalid OTP",
-
       });
-
     }
 
     // check expiry
     if (new Date() > user.otpExpiry) {
-
       return NextResponse.json({
-
         success: false,
         message: "OTP Expired",
-
       });
-
     }
 
-    // hash new password
-    const hashedPassword =
-      await bcrypt.hash(newPassword, 10);
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
 
     // update password
     user.password = hashedPassword;
-
     user.otp = "";
-
     await user.save();
 
     return NextResponse.json({
-
       success: true,
       message: "Password Reset Successful",
-
     });
 
   } catch (error) {
-
     return NextResponse.json({
-
       success: false,
       message: error.message,
-
     });
-
   }
 }
